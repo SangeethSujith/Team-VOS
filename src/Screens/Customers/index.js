8e303680b466fcdbc8d2c944e1eeb00f9b5141e1
Sangeeth Sujith <sangeethps2255@gmail.com>
2022-12-30 20:43:34 +0530

    Changes to customer api values

--- src/Screens/Customers/index.js
+++ src/Screens/Customers/index.js
@@ -27,9 +27,9 @@ const Customers = ({ navigation }) => {
     let Data = JSON.parse(userData)
     //console.log(Data.Userid);
     let body = {
-      userid: Data.Userid
+      user_id: Data.Userid
     }
-    axios.get(`${BASE_URL}/${CUSTOMERS}?userid=${Data.Userid}`,
+    axios.get(`${BASE_URL}/${CUSTOMERS}?user_id=${Data.Userid}`,
       qs.stringify(body)).then(async (response) => {
         setloader(false)
         await setstate1(response.data)
